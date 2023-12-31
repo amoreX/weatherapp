899261b76833a3674a2aebfbc2ca88d8a5ae292b
Md Nihal Rahman <rahmannihal72@gmail.com>
2023-12-31 18:44:46 +0530

    yuck walpoys

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import axios from 'axios';
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-let videoFilePath="/fog.mp4";
-let x=0
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,33 +17,18 @@ app.listen(port, ()=>{
 app.post("/submit",async(req,res)=>{
     var lat=req.body['lat'];
     var long=req.body['long'];
-    const apiurl="https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&current=temperature_2m"
+    const apiurl="https://api.open-meteo.com/v1/forecast?latitude="+lat+"&longitude="+long+"&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
     const weather= await axios.get(apiurl);
     let w=weather.data;
     let temp=w.current.temperature_2m+"°C";
     console.log(temp);
+    console.log(w)
     // console.log(w);
     // res.json(w);
-    let x=parseInt(temp,10);
-    if (x<0){
-        videoFilePath="/winter.mp4";
-        console.log(videoFilePath);
-    }
-    else if (x>=0 && x<15){
-        videoFilePath="/fog.mp4";
-        console.log(videoFilePath);
-    }
-    else{
-        videoFilePath="/summer.mp4";
-        console.log(videoFilePath);
-    }
+    res.render("index.ejs",{"temper":temp,"statement":"The Temperature:"});
     
-
-    res.render("index.ejs",{"temper":temp,"statement":"The Temperature:","videoFilePath":videoFilePath});
-    
-
 })
 app.get("/",(req,res)=>{
-    res.render("index.ejs",{"statement":" ","temper":" ","videoFilePath":"/home.mp4"});
+    res.render("index.ejs",{"statement":" ","temper":" "});
 
 })
