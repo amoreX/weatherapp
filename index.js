10098aeed6cc8bb108e9cc9c7f8a22bd2e7a46d3
N!hal <71655709+amoreX@users.noreply.github.com>
2024-01-01 23:25:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }));
 
